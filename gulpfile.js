bb5945c263a1650d8778e86d96db1b5c4e75d43f
Evgeniy Kuzovlev <evgeniy.kuzovlev@outlook.com>
2016-07-09 15:42:57 +0300

    replace rigger, add w3c for html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp')
 	, watch = require('gulp-watch')
 	, concat = require('gulp-concat')
-	// server
 	, browserSync = require('browser-sync').create()
+	, include = require("gulp-include")
+	, rimraf = require('rimraf') // remove dir
+	, ifElse = require('gulp-if-else')
+	, filter = require('gulp-filter')
+	// w3c
+	, w3cjs = require('gulp-w3cjs')
+	, through2 = require('through2')
 	// css
 	, scss = require('gulp-ruby-sass')
 	, sourcemaps = require('gulp-sourcemaps')
@@ -16,20 +22,24 @@ var gulp = require('gulp')
 	, spritesmith = require('gulp.spritesmith')
 	, merge = require('merge-stream')
 	// fonts
-	// , cssfont64 = require('gulp-cssfont64')
-	// , font64 = require('gulp-simplefont64')
 	, font2css = require('gulp-font2css').default
-	// other
-	, rigger = require('gulp-rigger') // include (//=)
-	, rimraf = require('rimraf') // remove dir
-	, ifElse = require('gulp-if-else')
 	;
 
 var isDev = true;
 
 var path = {
 	in: {
-		pages:   ['dev/pages/**/*.{html,txt}', '!dev/partials/**/*.html'],
+		pages:   ['dev/pages/**/*', '!dev/pages/partials/*'],
+		css:     ['dev/scss/**/*.scss'],
+		js:      ['dev/js/**/*.js'],
+		img:     ['dev/images/**/*.{jpg,jpeg,gif,png}'],
+		fonts:   ['dev/fonts/**/*.{woff,woff2}'],
+		fontsjs: ['dev/fonts/fonts.js'],
+		sprites: ['dev/sprites/**/*.{jpg,jpeg,gif,png}'],
+		plugins: ['dev/plugins/**/*.js']
+	},
+	watch: {
+		pages:   ['dev/pages/**/*'],
 		css:     ['dev/scss/**/*.scss'],
 		js:      ['dev/js/**/*.js'],
 		img:     ['dev/images/**/*.{jpg,jpeg,gif,png}'],
@@ -49,16 +59,6 @@ var path = {
 		sprites_s: 'dev/scss/',
 		plugins:   'build/js/'
 	},
-	watch: {
-		pages:   ['dev/pages/**/*.{html,txt}'],
-		css:     ['dev/scss/**/*.scss'],
-		js:      ['dev/js/**/*.js'],
-		img:     ['dev/images/**/*.{jpg,jpeg,gif,png}'],
-		fonts:   ['dev/fonts/**/*.{woff,woff2}'],
-		fontsjs: ['dev/fonts/fonts.js'],
-		sprites: ['dev/sprites/**/*.{jpg,jpeg,gif,png}'],
-		plugins: ['dev/plugins/**/*.js']
-	},
 	clean: './build'
 };
 
@@ -68,8 +68,8 @@ gulp.task('server', function () {
 		server: {
 			baseDir: './build'
 		},
-		// tunnel: true,
 		tunnel: false,
+		// tunnel: true,
 		host: 'localhost',
 		port: 9000,
 		logPrefix: "kio",
@@ -81,14 +81,18 @@ gulp.task('server', function () {
 
 // pages
 gulp.task('pages', function() {
+	fW3C = filter(['**/*.html', '!**/ajax/**'], {restore: true});
 	return gulp.src(path.in.pages)
-		.pipe(rigger())
+		.pipe(include())
+		.pipe(fW3C)
+		.pipe(w3cjs())
+		.pipe(fW3C.restore)
 		.pipe(gulp.dest(path.out.pages))
 		.pipe(browserSync.reload({stream: true}));
 });
 
 // styles
-gulp.task('styles', ['sprites'], function() {
+gulp.task('styles', function() {
 	return scss(path.in.css, {noCache: true, sourcemap: true})
 		.pipe(ifElse(isDev,
 			function() { return sourcemaps.init() }
@@ -103,7 +107,7 @@ gulp.task('styles', ['sprites'], function() {
 });
 
 // images
-gulp.task('images', ['sprites'], function() {
+gulp.task('images', function() {
 	return gulp.src(path.in.img)
 		.pipe(ifElse(!isDev,
 			function() { return imagemin({progressive: true, interlaced: true}) }
