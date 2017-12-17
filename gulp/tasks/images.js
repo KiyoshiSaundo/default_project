f2e1f6e0c722808b29a542778d0cc67f6bbd37b7
Evgeniy <kiyoshisaundo@gmail.com>
2017-12-17 12:44:18 +0300

    fix #4

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -3,19 +3,23 @@
 var gulp     = require('gulp'),
     watch    = require('gulp-watch'),
     gulpif   = require('gulp-if'),
+    filter   = require('gulp-filter'),
     imagemin = require('gulp-imagemin');
 
 module.exports = {
 	task: function(taskName, params) {
 		var pathIn  = [
-			params.path.in + '/images/**/*.{jpg,jpeg,gif,png}',
-			params.path.in + '/images_tmp/**/*.{jpg,jpeg,gif,png}'
+			params.path.in + '/images/**/*.{jpg,jpeg,gif,png,svg}',
+			params.path.in + '/images_tmp/**/*.{jpg,jpeg,gif,png,svg}'
 		];
 		var pathOut  = params.path.out + '/images';
 		var pathOutB = params.path.bitrix + '/images';
 
+		const f = filter(['**', '!**/*.svg'], {restore: true});
+
 		gulp.task(taskName, function() {
 			return gulp.src(pathIn)
+				.pipe(f)
 				.pipe(gulpif(
 					params.isImageMin,
 					imagemin({
@@ -23,6 +27,7 @@ module.exports = {
 						interlaced: true
 					})
 				))
+				.pipe(f.restore)
 				.pipe(gulpif(
 					params.isBitrix,
 					gulp.dest(pathOutB),
