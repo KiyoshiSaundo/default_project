bf5d44998de0c48ca2f5611feb41dcaf88181267
Evgeniy Kuzovlev <evgeniy.kuzovlev@yandex.ru>
2019-03-26 10:55:30 +0300

    fix #13

--- source/js/site.js
+++ source/js/site.js
@@ -1,37 +1,54 @@
-var vw = $(window).width();
-var vh = $(window).height();
+var vw = window.innerWidth;
+var vh = window.innerHeight;
+var sw = screen.width;
+var sh = screen.height;
+var pageScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
 	/* PARAMS */
 
-	$(window).resize(function() {
-		vw = $(window).width();
-		vh = $(window).height();
+	$(window).resize(function () {
+		vw = window.innerWidth;
+		vh = window.innerHeight;
+		sw = screen.width;
+		sh = screen.height;
+	});
+
+	$(document).scroll(function () {
+		pageScroll = window.pageYOffset || document.documentElement.scrollTop;
 	});
 
 	initInput();
 
 	/* ACTIONS */
 
-	// disable empty links
-	$('a[href="#"]').click(function(e){
+	// плавный скрол до элемента и отключение действий для ссылок "#"
+	$('[href^="#"]').click(function (e) {
 		e.preventDefault();
+		if ($(this).attr('href').length > 1) {
+			scroll2element(
+				$($(this).attr('href')),
+				1000,
+				0,
+				false
+			);
+		}
 	});
 
 	// placeholders
 	var placeholder = '';
-	$(document).on('focusin', 'input, textarea', function() {
+	$(document).on('focusin', 'input, textarea', function () {
 		placeholder = $(this).attr('placeholder');
 		$(this).attr('placeholder', '');
 	});
-	$(document).on('focusout', 'input, textarea', function() {
+	$(document).on('focusout', 'input, textarea', function () {
 		$(this).attr('placeholder', placeholder);
 	});
 	// end - placeholders
 
 	// ajax forms
-	$(document).on('submit', '[data-form-ajax]', function(e) {
+	$(document).on('submit', '[data-form-ajax]', function (e) {
 		e.preventDefault();
 		sendForm($(this));
 	});
@@ -55,13 +72,13 @@ function sendForm($el) {
 		contentType: false,
 		dataType: 'json',
 		// dataType: 'html',
-		beforeSend: function() {
+		beforeSend: function () {
 			hideErrorFields($form);
 			showBtnLoaded($btn);
 		},
-		success: function(data) {
+		success: function (data) {
 			// console.log('form success', data);
-			setTimeout(function() {
+			setTimeout(function () {
 				hideBtnLoaded($btn);
 				initInput();
 
@@ -76,9 +93,9 @@ function sendForm($el) {
 				}
 			}, 500);
 		},
-		error: function(data) {
+		error: function (data) {
 			// console.log('form error:', data);
-			setTimeout(function() {
+			setTimeout(function () {
 				hideErrorFields($form);
 				hideBtnLoaded($btn);
 			}, 500);
@@ -86,7 +103,7 @@ function sendForm($el) {
 	});
 
 	function showErrorFields($form, errors) {
-		$.each(errors, function(i, val) {
+		$.each(errors, function (i, val) {
 			$el = $form.find("[name='" + val + "']");
 			if ($el.length) $el.addClass('is-error');
 		});
@@ -103,8 +120,8 @@ function sendForm($el) {
 	}
 }
 
-function initInput(){
-	if ( $().datepicker ) {
+function initInput() {
+	if ($().datepicker) {
 		$('input.date').datepicker({
 			autoClose: true,
 			toggleSelected: false,
@@ -112,14 +129,39 @@ function initInput(){
 			minDate: new Date()
 		});
 	}
-	if ( $().mask ) {
-		$(".phone").mask("+7 (999) 999 99 99", {placeholder: "+7 (   )          "});
+	if ($().mask) {
+		$(".phone").mask("+7 (999) 999 99 99", { placeholder: "+7 (   )          " });
 	}
-	if ( $().styler ) {
-		setTimeout(function() {
+	if ($().styler) {
+		setTimeout(function () {
 			$("select:not(.nostyle), input[type='checkbox'], input[type='radio']").styler({
 				singleSelectzIndex: 10
 			});
 		}, 100);
+	}
+}
+
+/**
+ * Прокрутка страницы до определенного элемента, если он не в поле видимости
+ *
+ * @param  {jQuery} $el ссылка на объект
+ * @param  {string | number} speed скорость прокрутки
+ * @param  {number} offset смещение от начала страницы
+ * @param  {boolean} edges нужно ли учитывать видимость объекта
+ */
+function scroll2element($el, speed, offset, edges) {
+	if (speed == undefined) speed = 'slow';
+	if (offset == undefined) offset = 50;
+	if (edges == undefined) edges = true;
+
+	var scroll = $el.offset().top - offset,
+		topEdge = window.pageYOffset,
+		bottomEdge = window.pageYOffset + document.documentElement.clientHeight
+		bNeedScroll = edges ? (scroll < topEdge || scroll > bottomEdge) : true;
+
+	if (bNeedScroll) {
+		$('html, body').animate({
+			scrollTop: scroll+'px'
+		}, speed);
 	}
 }
